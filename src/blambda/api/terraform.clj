cb81740341ff47e54627f49d4eae244f49169062
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-07-12 11:03:10 +0500

    pprint

--- src/blambda/api/terraform.clj
+++ src/blambda/api/terraform.clj
@@ -71,7 +71,7 @@
 
 (defn write-config [{:keys [lambda-name tf-module-dir extra-tf-config target-dir]
                      :as opts}]
-  (println "OPTS:\t" opts)
+  (clojure.pprint/pprint opts)
   (let [opts (assoc opts
                     :lambda-filename (format "%s.zip" lambda-name))
         lambda-layer-config (generate-config opts)
