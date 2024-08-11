f4f49c2238d3c9e3ed6568d7e08ab94cf24c0363
Sasha <sasha_bogdanov_dev@yahoo.com>
2024-08-11 20:07:26 +0500

    Update terraform.clj

--- src/blambda/api/terraform.clj
+++ src/blambda/api/terraform.clj
@@ -85,7 +85,7 @@
       (doseq [f extra-tf-config
               :let [filename (fs/file-name f)
                     target (fs/file target-dir filename)
-                    content (selmer/render (slurp f) extra-tf-vars)]]
+                    content (selmer/render (slurp f) (assoc extra-tf-vars :lambda-name lambda-name)]]
         (println "Applying Terraform config" (str f))
         (fs/delete-if-exists target)
         (spit target content)))
